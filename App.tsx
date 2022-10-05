eadfc91e208b727a524f5faf08efa661dcf6bb56
Willochs316 <willochs316@gmail.com>
2022-10-05 19:20:56 +0100

    chore: initial commit

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {FC} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export const App: FC =  () =>  {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
